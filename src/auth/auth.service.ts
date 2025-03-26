7e12239b2efd0e26a1683f7de76ddf9297fea363
Thiago Almeida <thiagoalmeidadepaiva@gmail.com>
2025-03-26 13:46:04 -0300

    adding .env.example

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService, private jwtService: JwtService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
 
   async register(email: string, password: string, name?: string) {
     try {
-      const existingUser = await this.prisma.user.findUnique({ where: { email } });
+      const existingUser = await this.prisma.user.findUnique({
+        where: { email },
+      });
 
       if (existingUser) {
-        throw new Error("E-mail já cadastrado.");
+        throw new HttpException(
+          {
+            statusCode: HttpStatus.CONFLICT,
+            message: 'E-mail já cadastrado.',
+          },
+          HttpStatus.CONFLICT,
+        );
       }
 
       const hashedPassword = await bcrypt.hash(password, 10);
@@ -23,10 +34,17 @@ export class AuthService {
 
       return this.createToken(user);
     } catch (error) {
-      if (error.code === "P2002") {
-        throw new Error("Este e-mail já está em uso.");
+      if (error instanceof HttpException) {
+        throw error;  
       }
-      throw new Error("Erro ao registrar usuário.");
+      
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: 'Falha ao criar usuário',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
 
@@ -35,17 +53,17 @@ export class AuthService {
       const user = await this.prisma.user.findUnique({ where: { email } });
 
       if (!user) {
-        throw new Error("Credenciais inválidas.");
+        throw new Error('Credenciais inválidas.');
       }
 
       const passwordMatches = await bcrypt.compare(password, user.password);
       if (!passwordMatches) {
-        throw new Error("Credenciais inválidas.");
+        throw new Error('Credenciais inválidas.');
       }
 
       return this.createToken(user);
     } catch (error) {
-      throw new Error("Erro ao autenticar usuário.");
+      throw new Error('Erro ao autenticar usuário.');
     }
   }
 
